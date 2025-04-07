f2a66176536e35647958498f5906fadccdfa6a37
Easton <eastonmullins@usf.edu>
2025-04-06 22:03:00 -0400

    Task 1: Create a New Component

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Greeting from './components/Greeting';
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Greeting />
+    </>
+  );
+}
+
+export default App;
